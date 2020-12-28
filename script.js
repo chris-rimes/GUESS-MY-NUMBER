5dd6d76df8e75e9d2cb7ec7bc9f6350b2abcdf85
ckrimes <56796061+chris-rimes@users.noreply.github.com>
2020-12-28 10:41:09 -0700

    add css changes for win condition

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
+
     document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
 
     // When guess is too high
   } else if (guess > secretNumber) {
